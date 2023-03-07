03ec17dec4372e8c2bb98dbcb71eb9a176f44ec0
tuntinotuan <tuntino666@gmail.com>
2023-03-07 13:14:57 +0700

    component banner & callApi

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Fragment } from "react";
 import "swiper/scss";
+import Banner from "./components/banner/Banner";
 import MovieList from "./components/movie/MovieList";
 
 function App() {
@@ -9,33 +10,7 @@ function App() {
         <span className="text-primary">Home</span>
         <span>Movies</span>
       </header>
-      <section className="banner h-[500px] page-container mb-20">
-        <div className="w-full h-full rounded-lg relative">
-          <div className="overlay absolute inset-0 bg-gradient-to-t from-[rgba(0,0,0,0.5)] from-[rgba(0,0,0,0.5)] rounded-lg"></div>
-          <img
-            src="https://nld.mediacdn.vn/2019/4/25/3515432-endgamedek-15561710302491765206118.jpg"
-            alt=""
-            className="w-full h-full object-cover rounded-lg"
-          />
-          <div className="absolute left-5 bottom-5 w-full text-white">
-            <h2 className="font-bold text-3xl mb-5">Avengers: Endgame</h2>
-            <div className="flex items-center gap-x-3 mb-8">
-              <span className="py-2 px-4 border border-white rounded-md">
-                Adventure
-              </span>
-              <span className="py-2 px-4 border border-white rounded-md">
-                Adventure
-              </span>
-              <span className="py-2 px-4 border border-white rounded-md">
-                Adventure
-              </span>
-            </div>
-            <button className="py-3 px-6 rounded-lg bg-primary text-white font-medium">
-              Watch now
-            </button>
-          </div>
-        </div>
-      </section>
+      <Banner></Banner>
       <section className="movies-layout page-container pb-10">
         <h2 className="capitalize text-white mb-10 text-3xl font-bold">
           Now playing
