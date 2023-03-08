9f5c928e2e413e03ac4aefd962ff5631c9408851
tuntinotuan <tuntino666@gmail.com>
2023-03-09 00:06:17 +0700

    SU Routes

--- src/App.js
+++ src/App.js
@@ -1,34 +1,28 @@
 import { Fragment } from "react";
+import { Route, Routes } from "react-router-dom";
 import "swiper/scss";
 import Banner from "./components/banner/Banner";
-import MovieList from "./components/movie/MovieList";
+import Main from "./components/layout/Main";
+import HomePage from "./pages/HomePage";
+import MoviePage from "./pages/MoviePage";
 
 function App() {
   return (
     <Fragment>
-      <header className="header flex items-center justify-center gap-x-5 text-white py-10 mb-10">
-        <span className="text-primary">Home</span>
-        <span>Movies</span>
-      </header>
-      <Banner></Banner>
-      <section className="movies-layout page-container pb-10">
-        <h2 className="capitalize text-white mb-10 text-3xl font-bold">
-          Now playing
-        </h2>
-        <MovieList></MovieList>
-      </section>
-      <section className="movies-layout page-container pb-10">
-        <h2 className="capitalize text-white mb-10 text-3xl font-bold">
-          Top rated
-        </h2>
-        <MovieList type="top_rated"></MovieList>
-      </section>
-      <section className="movies-layout page-container pb-10">
-        <h2 className="capitalize text-white mb-10 text-3xl font-bold">
-          Trending
-        </h2>
-        <MovieList type="popular"></MovieList>
-      </section>
+      <Routes>
+        <Route element={<Main></Main>}>
+          <Route
+            path="/"
+            element={
+              <>
+                <Banner></Banner>
+                <HomePage></HomePage>
+              </>
+            }
+          ></Route>
+          <Route path="/movies" element={<MoviePage></MoviePage>}></Route>
+        </Route>
+      </Routes>
     </Fragment>
   );
 }
---
+++ src/pages/MoviePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const MoviePage = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut at debitis,
+      rem veritatis vero nam quae itaque quos aspernatur delectus ipsa aliquam
+      deserunt nesciunt est animi pariatur minus perspiciatis. Praesentium!
+    </div>
+  );
+};
+
+export default MoviePage;
