8044b27ad25de978c92a6644a6951588b30550f9
tuntinotuan <tuntino666@gmail.com>
2023-03-09 01:18:57 +0700

    fetchingApi => MoviePage

--- src/pages/MoviePage.js
+++ src/pages/MoviePage.js
@@ -1,11 +1,48 @@
 import React from "react";
+import MovieList from "../components/movie/MovieList";
+import useSWR from "swr";
+import { fetcher } from "../config";
+import MovieCard from "../components/movie/MovieCard";
 
 const MoviePage = () => {
+  const { data } = useSWR(
+    `https://api.themoviedb.org/3/movie/popular?api_key=3d822c02b85d7e4e8fb9dd03e653e84a`,
+    fetcher
+  );
+  const movies = data?.results || [];
   return (
-    <div>
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut at debitis,
-      rem veritatis vero nam quae itaque quos aspernatur delectus ipsa aliquam
-      deserunt nesciunt est animi pariatur minus perspiciatis. Praesentium!
+    <div className="py-10">
+      <div className="flex mb-10">
+        <div className="flex-1">
+          <input
+            type="text"
+            className="w-full p-4 bg-slate-800 text-white outline-none"
+            placeholder="Type here to search..."
+          />
+        </div>
+        <button className="p-4 bg-primary text-white">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+            />
+          </svg>
+        </button>
+      </div>
+      <div className="grid grid-cols-4 gap-10">
+        {movies.length > 0 &&
+          movies.map((item) => (
+            <MovieCard key={item.id} item={item}></MovieCard>
+          ))}
+      </div>
     </div>
   );
 };
