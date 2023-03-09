4cb0d086719b7dcc717d4db9b411b158bfd25c35
tuntinotuan <tuntino666@gmail.com>
2023-03-10 00:57:26 +0700

    getApi for MovieDetailsPage with =>useNavigate

--- src/components/movie/MovieCard.js
+++ src/components/movie/MovieCard.js
@@ -1,8 +1,10 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const MovieCard = ({ item }) => {
-  const { title, vote_average, release_date, poster_path, backdrop_path } =
+  const { title, vote_average, release_date, poster_path, backdrop_path, id } =
     item;
+  const navigate = useNavigate();
   return (
     <div className="movie-card flex flex-col rounded-lg p-3 bg-slate-800 text-white h-full select-none">
       <img
@@ -16,7 +18,10 @@ const MovieCard = ({ item }) => {
           <span>{new Date(release_date).getFullYear()}</span>
           <span>{vote_average}</span>
         </div>
-        <button className="py-3 px-6 rounded-lg capitalize bg-primary w-full mt-auto">
+        <button
+          onClick={() => navigate(`/movie/${id}`)}
+          className="py-3 px-6 rounded-lg capitalize bg-primary w-full mt-auto"
+        >
           Watch now
         </button>
       </div>
--- src/config.js
+++ src/config.js
@@ -1,1 +1,2 @@
 export const fetcher = (...args) => fetch(...args).then((res) => res.json());
+export const apiKey = "3d822c02b85d7e4e8fb9dd03e653e84a";
