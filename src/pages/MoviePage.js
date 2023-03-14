d0ab4f7210c133cf64c9336d0cb932023e709be5
tuntinotuan <tuntino666@gmail.com>
2023-03-14 13:34:35 +0700

    MoviePage => simple paginate

--- src/pages/MoviePage.js
+++ src/pages/MoviePage.js
@@ -5,10 +5,13 @@ import { fetcher } from "../config";
 import MovieCard from "../components/movie/MovieCard";
 import useDebounce from "../hooks/useDebounce";
 // https://api.themoviedb.org/3/search/movie?api_key=3d822c02b85d7e4e8fb9dd03e653e84a
+
+const pageCount = 5;
 const MoviePage = () => {
+  const [nextPage, setNextPage] = useState(1);
   const [filter, setFilter] = useState("");
   const [url, setUrl] = useState(
-    `https://api.themoviedb.org/3/movie/popular?api_key=3d822c02b85d7e4e8fb9dd03e653e84a`
+    `https://api.themoviedb.org/3/movie/popular?api_key=3d822c02b85d7e4e8fb9dd03e653e84a&page=${nextPage}`
   );
   const filterDebounce = useDebounce(filter, 500);
   const handleFilterChange = (e) => {
@@ -19,14 +22,14 @@ const MoviePage = () => {
   useEffect(() => {
     if (filterDebounce) {
       setUrl(
-        `https://api.themoviedb.org/3/search/movie?api_key=3d822c02b85d7e4e8fb9dd03e653e84a&query=${filter}`
+        `https://api.themoviedb.org/3/search/movie?api_key=3d822c02b85d7e4e8fb9dd03e653e84a&query=${filter}&page=${nextPage}`
       );
     } else {
       setUrl(
-        "https://api.themoviedb.org/3/movie/popular?api_key=3d822c02b85d7e4e8fb9dd03e653e84a"
+        `https://api.themoviedb.org/3/movie/popular?api_key=3d822c02b85d7e4e8fb9dd03e653e84a&page=${nextPage}`
       );
     }
-  }, [filterDebounce]);
+  }, [filterDebounce, nextPage]);
 
   const movies = data?.results || [];
   return (
@@ -68,37 +71,49 @@ const MoviePage = () => {
           ))}
       </div>
       <div className="flex items-center justify-center mt-10 gap-x-5">
-        <span className="cursor-pointer">
+        <span
+          className="cursor-pointer"
+          onClick={() => setNextPage(nextPage - 1)}
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             stroke="currentColor"
-            class="w-6 h-6"
+            className="w-6 h-6"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M15.75 19.5L8.25 12l7.5-7.5"
             />
           </svg>
         </span>
-        <span className="cursor-pointer inline-block py-2 px-4 rounded leading-none bg-white text-slate-900">
-          1
-        </span>
-        <span className="cursor-pointer">
+        {new Array(pageCount).fill(0).map((item, index) => (
+          <span
+            key={index}
+            className="cursor-pointer inline-block py-2 px-4 rounded leading-none bg-white text-slate-900"
+            onClick={() => setNextPage(index + 1)}
+          >
+            {index + 1}
+          </span>
+        ))}
+        <span
+          className="cursor-pointer"
+          onClick={() => setNextPage(nextPage + 1)}
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             stroke="currentColor"
-            class="w-6 h-6"
+            className="w-6 h-6"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M8.25 4.5l7.5 7.5-7.5 7.5"
             />
           </svg>
